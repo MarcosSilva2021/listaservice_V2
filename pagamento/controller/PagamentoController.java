f0cfa9b010516e7fadd44e3acceb4bf2fc654463
marcosdsj <89165879+MarcosSilva2021@users.noreply.github.com>
2024-05-25 11:09:53 -0300

    Delete pagamento directory

--- pagamento/controller/PagamentoController.java
+++
@@ -1,5 +0,0 @@
-package br.edu.unichristus.listaservice.pagamento.controller;
-
-public class PagamentoController {
-
-}
--- pagamento/service/PagamentoService.java
+++
@@ -1,5 +0,0 @@
-package br.edu.unichristus.listaservice.pagamento.service;
-
-public class PagamentoService {
-
-}
